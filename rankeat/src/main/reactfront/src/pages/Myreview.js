3eac2ffa835b760c9760a2a22397f276caf40e99
binys21 <binys1121@ewhain.net>
2022-08-22 18:35:48 +0900

    내가쓴리뷰

---
+++ rankeat/src/main/reactfront/src/pages/Myreview.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import './App.css';
+import Box from "./Box"
+
+function Myreview() {
+    return (
+        <main>
+            <div className="header">
+                <header>
+                    <h1>RankEat</h1>
+                </header>
+            </div>
+            <Box/>
+            <Box/>
+            <Box/>
+
+
+        </main>
+    );
+}
+
+export default Myreview;
