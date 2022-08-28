e983dc9827388e48fd89b2bb69edb70b7d7d7fad
binys21 <binys1121@ewhain.net>
2022-08-28 13:45:43 +0900

    개인정보수정2

--- rankeat/src/main/reactfront/src/pages/Personal.js
+++ rankeat/src/main/reactfront/src/pages/Personal.js
@@ -11,6 +11,30 @@ export default function Personal()
 
     const onChange = (e) => {
           setNick({nickname:e.target.value});
+
+        UpdateNick = () => {
+            fetch('API주소', {
+                method: 'POST',
+                body: JSON.stringify({
+                    "phone" : "01012345678",
+                    "name" : "nickname"
+                }),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+                .then(response => response.json())
+                .then(response => {
+                    if (response.message === '닉네임 수정 성공') {
+                        return alert('변경되었습니다.');
+                    } else if (response.message === '필수 데이터 누락') {
+                        alert('필수 정보를 모두 입력해주세요.');
+                    } else if (response.message === '이미 존재하는 닉네임'){
+                        alert('이미 존재하는 닉네임입니다.');
+                    }
+                });
+        };
+
     return (
 
         <main>
@@ -28,12 +52,10 @@ export default function Personal()
                 </div>
                 <div className='nick'>
                     <p>닉네임</p>
-                    <p><input type="text" placeholder="새로운 닉네임 입력" /></p>
-                    <button>중복확인</button>
+                    <p><input type="text" placeholder="새로운 닉네임 입력" onChange={onNickHandler} /></p
                 </div>
-                <button onClick={function(){
-                    alert('수정되었습니다.');
-                }}  type="button" >수정하기</button>
+
+                <button onClick={UpdateNick}  type="button" >수정하기</button>
             </div>
 
         </main>
