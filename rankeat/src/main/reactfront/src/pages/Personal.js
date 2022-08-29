88cd33d4fb54f7652e478f13c75814da67aec82b
binys21 <binys1121@ewhain.net>
2022-08-29 16:47:16 +0900

    개인정보수정3

--- rankeat/src/main/reactfront/src/pages/Personal.js
+++ rankeat/src/main/reactfront/src/pages/Personal.js
@@ -2,7 +2,8 @@ import '../css/Personal.css'
 import React, {useState} from "react";
 import AppLayout from "./AppLayout";
 
-export default function Personal()
+function Personal() {
+
     const [Nick, setNick] = useState('')
 
     const onNickHandler = (e) => {
@@ -10,57 +11,51 @@ export default function Personal()
     }
 
     const onChange = (e) => {
-          setNick({nickname:e.target.value});
-
-        UpdateNick = () => {
-            fetch("http://localhost:8080/user/modify", {
-                method: 'POST',
-                body: JSON.stringify({
-                    "phone" : "01012345678",
-                    "name" : "nickname"
-                }),
-                headers: {
-                    "Content-Type": "application/json"
+        setNick({nickname: e.target.value})
+    }
+    /*
+    const UpdateNick = (e) => {
+        fetch("http://localhost:8080/user/modify", {
+            method: 'POST',
+            body: JSON.stringify({
+                "phone" : "01012345678",
+                "name" : "nickname"
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(response => response.json())
+            .then(response => {
+                if (response.message === '닉네임 수정 성공') {
+                    setNick(e.currentTarget.value)
+                    return alert('변경되었습니다.');
+                } else if (response.message === '필수 데이터 누락') {
+                    alert('필수 정보를 모두 입력해주세요.');
+                } else if (response.message === '이미 존재하는 닉네임'){
+                    alert('이미 존재하는 닉네임입니다.');
                 }
-            })
-                .then(response => response.json())
-                .then(response => {
-                    if (response.message === '닉네임 수정 성공') {
-                        setNick(e.currentTarget.value)
-                        return alert('변경되었습니다.');
-                    } else if (response.message === '필수 데이터 누락') {
-                        alert('필수 정보를 모두 입력해주세요.');
-                    } else if (response.message === '이미 존재하는 닉네임'){
-                        alert('이미 존재하는 닉네임입니다.');
-                    }
-                });
+            });
 
-        };
+    };
 
-    return (
+     */
+return (
 
-        <main>
-            <AppLayout/>
-            <div className='box'>
-                개인정보수정
-                <div>
-
-
-                </div>
-                <div className='small'>
-                    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-
-                    <span class="material-symbols-outlined"> account_circle</span>
-                </div>
+            <div>
+                <AppLayout/>
+                <div className='minibox'>
+                    개인정보수정
                 <div className='nick'>
                     <p>닉네임</p>
-                    <p><input type="text" placeholder="새로운 닉네임 입력" onChange={onNickHandler}
-                    /></p
-                </div>
+                    <p><input type="text" placeholder="새로운 닉네임 입력" onChange={onNickHandler}/></p>
 
-                <button onClick={UpdateNick}  type="button" >수정하기</button>
+                <button /*onClick={UpdateNick}*/  type="button" >수정하기</button>
+                </div>
+                </div>
             </div>
 
-        </main>
-    )
-}
+ )}
+
+
+export default Personal;
