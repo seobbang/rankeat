656a878a02abb4151f4fa5a6ae3e9f74a22f8cc7
SOLJI YOON <102536809+Solji5@users.noreply.github.com>
2022-08-29 11:28:59 +0900

    Add files via upload

---
+++ rankeat/src/pages/MainProduct.js
@@ -0,0 +1,11 @@
+import react from "react";
+import reactDOM from "react-dom";
+
+const MainProduct =()=>{
+
+  return(
+    <div className = "MainProduct-container">
+      <div className = "MainProduct"></div>
+    </div>
+  )
+}
