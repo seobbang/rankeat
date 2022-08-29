bac0b2b83b4d450ec49f9c2dd72982f0842c9471
binys21 <binys1121@ewhain.net>
2022-08-29 12:45:34 +0900

    내가쓴리뷰2

--- rankeat/src/main/reactfront/src/pages/Box.js
+++ rankeat/src/main/reactfront/src/pages/Box.js
@@ -2,39 +2,40 @@ import React from 'react';
 import axios from 'axios';
 
 function Box(){
+    const Fetch = () => {
 
-    function searchApi() {
-        const url = "http://localhost:8080/login";
-        axios.get(url)
-            .then(function(response) {
-                setPhotos(response.data);
-                console.log("성공");
-            })
-            .catch(function(error) {
-                console.log("실패");
-            })
+        const [posts, setPosts] = useState([])
 
-    }
+        useEffect(() => {
+            fetch("http://localhost:8080/login", {method: 'GET'})
+                .then(res => res.json())
+                .then(data => {
+                    setPosts(data)
+                })
+        },[])
 
     return(
         <div className="box">
             <div className="pic">
                 <div className="dis">
-                    가격:<br></br>웨이팅:<br></br>맛:<br></br>위생:<br></br>서비스:
+                    가격:{posts.price}<br></br>웨이팅:{posts.waiting}<br></br>
+                    맛:{posts.taste}<br></br>위생:{posts.clean}<br></br>서비스:{posts.service}
                 </div>
             </div>
             <div className="nm">
-                식당이름</div>
+                식당이름자리 {posts.storename}</div>
+                {posts.dalpoint} <br></br>
+                {posts.contents}
             <div className="tag">
-                #_____</div>
+                {posts.tag1}</div>
             <div className="tag">
-                #_____</div>
+                {posts.tag2}</div>
             <div className="tag">
-                #_____ </div>
+                {posts.tag3} </div>
 
             <div className="tebox"> </div>
             <span class="material-symbols-outlined">
-        delete
+             delete
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 </span>
         </div>
