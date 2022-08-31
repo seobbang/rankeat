bec5924a33c82f9bdc464f692b790b8ca1e4a6f6
binys21 <binys1121@ewhain.net>
2022-08-31 15:12:22 +0900

    내가쓴리뷰2

--- rankeat/src/main/reactfront/src/pages/Box.js
+++ rankeat/src/main/reactfront/src/pages/Box.js
@@ -1,8 +1,8 @@
 import * as React from 'react';
 
-
+/*
 function Box(){
-    /*
+
 }
     const Fetch = () => {
 
@@ -18,37 +18,12 @@ function Box(){
                     setPosts(data)
                 })
         },[])
-*/
+
     return(
-        <div className="box">
-            <div className="pic">
-                {/*
-                <div className="dis">
-                    가격:{posts.price}<br></br>웨이팅:{posts.waiting}<br></br>
-                    맛:{posts.taste}<br></br>위생:{posts.clean}<br></br>서비스:{posts.service}
-                </div>
-            </div>
-            <div className="nm">
-                식당이름자리 {posts.storename}</div>
-                {posts.dalpoint} <br></br>
-                {posts.contents}
-            <div className="tag">
-                {posts.tag1}</div>
-            <div className="tag">
-                {posts.tag2}</div>
-            <div className="tag">
-                {posts.tag3} </div>
-            */}
+        <div> 식당 이름</div>
 
-            <div className="tebox"> </div>
-            <span class="material-symbols-outlined">
-             delete
-        <link rel="stylesheet"
-              href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-</span>
-        </div>
-        </div>
+
+
     )
-}
     export default Box;
-
+*/
