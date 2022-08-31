56bb32762a1ddd5bc89181aa6ac84b2c0195c9af
AnHeeJae <gmlwo000810@gmail.com>
2022-08-31 21:52:26 +0900

    ViewReview.js 수정

--- rankeat/src/main/reactfront/src/components/ViewReview.js
+++ rankeat/src/main/reactfront/src/components/ViewReview.js
@@ -103,13 +103,17 @@ const ViewReview = (dt) => {
         return assessment;
     }
 
+
+
+ //============================================================================================
+
     return (
         <>
           <div className="title">
              Review
           </div>
-{/* 
-          {dt.map((it) => (
+
+          { dt.reviewList.map((it) => (
                 <div className="reviewBox">
            
                       <div className="nick">
@@ -135,31 +139,13 @@ const ViewReview = (dt) => {
                       </div>
       
                       <div className="statement">
-                          
+                          {it.contents}
                       </div>
                   
                 </div>
-          ))} */}
-{/* 
-          <div className="reviewBox">
-           
-                <div className="nick">
-                    <span>닉네임</span>
-                </div>
+          )) 
+          } 
 
-                <div className="assessment">
-                    <span>가격 : </span> <br/>
-                    <span>웨이팅 : </span> <br/>
-                    <span>맛 :</span>  <br/>
-                    <span>위생 :</span>  <br/>
-                    <span>서비스: </span>
-                </div>
-
-                <div className="statement">
-                    
-                </div>
-            
-          </div> */}
         </>
     );
 }
