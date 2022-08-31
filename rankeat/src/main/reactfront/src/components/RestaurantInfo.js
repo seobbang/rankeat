9462a487ffbacc64142b3e290b1da3d63bc9a64e
AnHeeJae <gmlwo000810@gmail.com>
2022-09-01 01:36:31 +0900

    RestaurantInfo 컴포넌트 수정

--- rankeat/src/main/reactfront/src/components/RestaurantInfo.js
+++ rankeat/src/main/reactfront/src/components/RestaurantInfo.js
@@ -1,7 +1,51 @@
 /* 업체 정보 */
-import React from "react";
 
-const RestaurantInfo = () => {
+const RestaurantInfo = (dt) => {
+
+    function numToKeword (tag) {
+
+        let keyword  ='';
+
+        switch (tag) {
+            case "1":
+                keyword = '가성비 좋음';
+                break;
+            case '2':
+                keyword = '넉넉한 밥양';
+                break;
+            case "3":
+                keyword = '밥약에 좋음';
+                break;
+            case "4":
+                keyword = '카공에 적합';
+                break;
+            case "5":
+                keyword = '미팅에 적합';
+                break;
+            case "6":
+                keyword = '혼밥에 적당';
+                break;
+            case "7":
+                keyword = '늦게까지 함';
+                break;
+            case "8":
+                keyword = '특별한 날에';
+                break;
+            case "9":
+                keyword = '매장이 깨끗';
+                break;
+            case "10":
+                keyword = '재료가 신선';
+                break;                
+            default:
+                keyword = '';
+                break;
+        }
+
+        return keyword;
+    }
+
+  //=============================================================================
 
     return (
         <div id="restaurantInfo">
@@ -11,14 +55,18 @@ const RestaurantInfo = () => {
                 </div>
 
                 <div className="keywords">
-                    <span>#키워드1</span>
-                    <span>#키워드2</span>
-                    <span>#키워드3</span>
+                    <span>#{numToKeword(dt.store.tag1)}</span>   
+                    <span>#{numToKeword(dt.store.tag2)}</span>
+                    <span>#{numToKeword(dt.store.tag3)}</span>
                 </div>
 
                 <div className="other_info"> 
-                    <span>주소</span> 
-                    <span>전화번호</span> 
+                   <div> 
+                      <span className="category">주소</span> <span className="info">{dt.store.address}</span> 
+                   </div>
+                   <div>
+                      <span className="category">전화번호</span> <span className="info">{dt.store.tel}</span>
+                   </div> 
                 </div>
 
         </div>
