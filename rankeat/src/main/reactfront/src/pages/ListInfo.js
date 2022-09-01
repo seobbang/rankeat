a84b4c17780ef46b71eaa5036b39a585ce311740
binys21 <binys1121@ewhain.net>
2022-09-02 01:42:20 +0900

    리스트상세정보

--- rankeat/src/main/reactfront/src/pages/ListInfo.js
+++ rankeat/src/main/reactfront/src/pages/ListInfo.js
@@ -2,16 +2,55 @@ import AppLayout from "./AppLayout";
 import '../css/ListInfo.css';
 import React, { useState, useEffect } from 'react';
 
-function ListInfo(){
-    return(
-        <div className='title'>
-            <AppLayout/>
-            리스트 이름
-            <div className='restbox'>
-                식당이름
-            </div>
-        </div>
-    )
-};
 
+export default function ListInfo() {
+    return (
+        <main>
+            리스트이름
+            <br></br>
+
+            <div className='first'>
+                <h3>식당이름</h3>
+                <div class="img">👩‍💻</div>
+            </div>
+
+            <div className='second'>
+                <h3>식당이름</h3>
+                <div class="img">👩‍💻</div>
+            </div>
+
+            <div className='third'>
+                <h3>식당이름</h3>
+                <div class="img">👩‍💻</div>
+            </div>
+
+            <div className='forth'>
+                <h3>식당이름</h3>
+                <div class="img">👩‍💻</div>
+            </div>
+
+            <div className='fifth'>
+                <h3>식당이름</h3>
+                <div class="img">👩‍💻</div>
+            </div>
+
+            <div className='six'>
+                <h3>식당이름</h3>
+                <div class="img">👩‍💻</div>
+            </div>
+
+            <div className='seven'>
+                <h3>식당이름</h3>
+                <div class="img">👩‍💻</div>
+            </div>
+
+            <div className='seven'>
+                <h3>식당이름</h3>
+                <div class="img">👩‍💻</div>
+            </div>
+
+        </main>
+
+    )
+}
 export default ListInfo;
