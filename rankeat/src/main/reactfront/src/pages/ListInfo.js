2ce1c2d03d5912cc4db770affe0d5b32418ed941
binys21 <binys1121@ewhain.net>
2022-09-02 16:49:26 +0900

    리스트상세정보2

--- rankeat/src/main/reactfront/src/pages/ListInfo.js
+++ rankeat/src/main/reactfront/src/pages/ListInfo.js
@@ -73,3 +73,97 @@ import { IoMoon } from "react-icons/io5"
     )
 }
 export default ListInfo;
+
+
+/*
+ function ListInfo() {
+    return (
+    const list_info = {
+            method: "GET",
+            body: JSON.stringify({
+            "dalpoint": "3.51",
+                "storename": "식당이름",
+                "tag1" : "1",
+                "tag2" : "2",
+                "tag3" : "3"
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+        fetch("http://localhost:8080//mylist/list/:listnum")
+            .then(response => {
+                return response.json();
+            }
+            .then(json => {
+                    this.props.history.push("/DetailInfo")
+                }
+            });
+    };
+        <div className='main'>
+            <AppLayout/>
+             <div className='liname'>리스트이름</div>
+            <br></br>
+
+            <div className='first'>
+                <h3>식당이름</h3>
+                <div class="img"></div>
+                <IoMoon/> 3.5
+                <div className='tag'>{data.tag1}<br></br>{data.tag2}<br></br>{data.tag3}</div>
+                </div>
+            </div>
+
+            <div className='second'>
+                <h3>식당이름</h3>
+                <div class="img"></div>
+                <IoMoon/> 3.5
+                <div className='tag'>{data.tag1}<br></br>{data.tag2}<br></br>{data.tag3}</div>
+            </div>
+
+            <div className='third'>
+                <h3>식당이름</h3>
+                <div class="img"></div>
+                <IoMoon/> 3.5
+                <div className='tag'>{data.tag1}<br></br>{data.tag2}<br></br>{data.tag3}</div>
+            </div>
+
+            <div className='forth'>
+                <h3>식당이름</h3>
+                <div class="img"></div>
+                <IoMoon/> 3.5
+                <div className='tag'>{data.tag1}<br></br>{data.tag2}<br></br>{data.tag3}</div>
+            </div>
+
+            <div className='fifth'>
+                <h3>식당이름</h3>
+                <div class="img"></div>
+                <IoMoon/> 3.5
+                <div className='tag'>{data.tag1}<br></br>{data.tag2}<br></br>{data.tag3}</div>
+            </div>
+
+            <div className='six'>
+                <h3>식당이름</h3>
+                <div class="img"></div>
+                <IoMoon/> 3.5
+                <div className='tag'>{data.tag1}<br></br>{data.tag2}<br></br>{data.tag3}</div>
+            </div>
+
+            <div className='seven'>
+                <h3>식당이름</h3>
+                <div class="img"></div>
+               <IoMoon/> 3.5
+                <div className='tag'>{data.tag1}<br></br>{data.tag2}<br></br>{data.tag3}</div>
+            </div>
+
+            <div className='eight'>
+                <h3>식당이름</h3>
+                <div class="img"></div>
+                <IoMoon/> 3.5
+                <div className='tag'>{data.tag1}<br></br>{data.tag2}<br></br>{data.tag3}</div>
+            </div>
+
+        </div>
+
+    )
+}
+export default ListInfo;*/
