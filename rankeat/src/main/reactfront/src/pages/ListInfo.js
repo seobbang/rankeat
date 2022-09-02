984e7613d45b61f8b4ab0a0b598719d19f9e70fe
binys21 <binys1121@ewhain.net>
2022-09-02 16:15:46 +0900

    리스트상세정보2

--- rankeat/src/main/reactfront/src/pages/ListInfo.js
+++ rankeat/src/main/reactfront/src/pages/ListInfo.js
@@ -7,62 +7,63 @@ import { IoMoon } from "react-icons/io5"
 export default function ListInfo() {
     return (
         <div className='main'>
+            <AppLayout/>
             리스트이름
             <br></br>
 
             <div className='first'>
                 <h3>식당이름</h3>
                 <div class="img">👩‍💻</div>
-                <div className={"dalPoint"}><IoMoon/> 3.5 </div>
+                <IoMoon/> 3.5
                 <div className='tag'>#</div>
             </div>
 
             <div className='second'>
                 <h3>식당이름</h3>
                 <div class="img">👩‍💻</div>
-                <div className={"dalPoint"}><IoMoon/> 3.5 </div>
+                <IoMoon/> 3.5
                 <div className='tag'>#</div>
             </div>
 
             <div className='third'>
                 <h3>식당이름</h3>
                 <div class="img">👩‍💻</div>
-                <div className={"dalPoint"}><IoMoon/> 3.5 </div>
+                <IoMoon/> 3.5
                 <div className='tag'>#</div>
             </div>
 
             <div className='forth'>
                 <h3>식당이름</h3>
                 <div class="img">👩‍💻</div>
-                <div className={"dalPoint"}><IoMoon/> 3.5 </div>
+                <IoMoon/> 3.5
                 <div className='tag'>#</div>
             </div>
 
             <div className='fifth'>
                 <h3>식당이름</h3>
                 <div class="img">👩‍💻</div>
-                <div className={"dalPoint"}><IoMoon/> 3.5 </div>
+                <IoMoon/> 3.5
                 <div className='tag'>#</div>
             </div>
 
             <div className='six'>
                 <h3>식당이름</h3>
                 <div class="img">👩‍💻</div>
-                <div className={"dalPoint"}><IoMoon/> 3.5 </div>
+                <IoMoon/> 3.5
                 <div className='tag'>#</div>
             </div>
 
             <div className='seven'>
                 <h3>식당이름</h3>
                 <div class="img">👩‍💻</div>
-                <div className={"dalPoint"}><IoMoon/> 3.5 </div>
+               <IoMoon/> 3.5
                 <div className='tag'>#</div>
             </div>
 
             <div className='seven'>
                 <h3>식당이름</h3>
                 <div class="img">👩‍💻</div>
-                <div className={"dalPoint"}><IoMoon/> 3.5 </div>
+                <IoMoon/> 3.5
                 <div className='tag'>#</div>
             </div>
 
